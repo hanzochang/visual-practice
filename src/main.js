caff2c2491b06766e8772627cf7bf06fa61df0ba
hanzochang <hanzo.chan@gmail.com>
2017-11-09 10:11:06 +0900

    test uniform shader

--- src/main.js
+++ src/main.js
@@ -1,20 +1,40 @@
 import * as THREE from 'three';
 import ThreeSceneFactory from './three/three_scene_factory';
+import * as fragmentS from './test.frag';
+import * as vertextS from './test.vert';
 
 window.addEventListener('DOMContentLoaded', () => {
   let three = new ThreeSceneFactory(window.innerWidth, window.innerHeight);
   three.buildAll();
+  three.camera.position.z = 1;
 
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-  const box = new THREE.Mesh(geometry, material);
+  const geometry = new THREE.PlaneBufferGeometry(6, 2);
+
+  const unifor = {
+    time: { type: 'f', value: 1.0 },
+    resolution: { type: 'v2', value: new THREE.Vector2() },
+    mouse: { type: 'v2', value: new THREE.Vector2() },
+  };
+
+  let material = new THREE.ShaderMaterial({
+    uniforms: unifor,
+    vertexShader: vertextS,
+    fragmentShader: fragmentS,
+  });
+
   const light = new THREE.DirectionalLight(0xffffff);
-
   light.position.set(1, 1, 1);
-  box.position.z = -5;
 
+  const mat = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+  const geo2 = new THREE.BoxGeometry(1, 1, 1);
+  const box = new THREE.Mesh(geo2, mat);
+  const mesh = new THREE.Mesh(geometry, mat);
+
+  box.position.z = -5;
+  mesh.position.z = -6;
   three.scene.add(box);
   three.scene.add(light);
+  three.scene.add(mesh);
 
   const tick = () => {
     requestAnimationFrame(tick);
